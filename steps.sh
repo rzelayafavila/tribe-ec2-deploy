77989ca8eff19e4c2e20cde41cd47a13936afca1
Casey Greene <cgreene@users.noreply.github.com>
2014-11-28 08:00:15 -0500

    add user

--- steps.sh
+++ steps.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get -y -q install mercurial build-essential python python-dev python-distribute python-pip nginx supervisor
 sudo pip install uwsgi
+sudo adduser tribe www-data --disabled-login --system
 
